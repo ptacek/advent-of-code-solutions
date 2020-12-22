46a2590e857a602d84acc4135fb270035cff25d7
Daniel Ptáček <daniel.ptacek@vodafone.com>
2020-12-22 14:48:03 +0100

    WIP

--- Day10/AdapterArray.py
+++ Day10/AdapterArray.py
@@ -16,16 +16,33 @@ def countDifferences(adapters):
 
     return differences
 
+# Part TWO
+def countArrangements(adapters, i, ways):
+    joltage = adapters[i]
+
+    if i + 1 < len(adapters):
+        ways = countArrangements(adapters, i + 1, ways)
+    
+    if i + 2 < len(adapters) and adapters[i + 2] - joltage <= 3:
+        ways += 1
+        ways = countArrangements(adapters, i + 2, ways)
+    
+    if i + 3 < len(adapters) and adapters[i + 3] - joltage <= 3:
+        ways += 1
+        ways = countArrangements(adapters, i + 3, ways)
+
+    return ways
+
 file = open(sys.argv[1], 'r')
 lines = file.readlines()
 
-adapters = list(map(lambda x: int(x), lines))
+adapters = [0] + list(map(lambda x: int(x), lines))
 adapters.sort()
 
 print(adapters)
 
 differences = countDifferences(adapters)
 print("x = {}\ny = {}\n1x * 3y = {}".format(differences[1], differences[3], differences[1] * differences[3]))
-
+print(countArrangements(adapters, 0, 1))
 
 file.close()
